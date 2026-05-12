78c857d2d58a8d82931b5820cc8ed2373eb89966
xu <2813519505@qq.com>
2026-05-12 16:13:57 +0800

    添加乐观锁解决秒杀券超卖业务代码实现

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -58,7 +58,11 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存不足！");
         }
         //扣减库存
-        boolean success = seckillVoucherServicel.update().setSql("stock=stock-1").eq("voucher_id", voucherId).update();
+        boolean success = seckillVoucherServicel.update()
+                .setSql("stock=stock-1")
+                .eq("voucher_id", voucherId)
+                .gt("stock",0)
+                .update();
         if (!success) {
             //扣减失败
             return Result.fail("库存不足！");
